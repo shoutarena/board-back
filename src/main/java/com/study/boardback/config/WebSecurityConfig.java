e7283b41e75e7b33a7287d1f5f8bb8dd49fcaeaf
Lamie <flyskwon@naver.com>
2024-05-02 18:21:43 +0900

    jwt and security 구현

---
+++ src/main/java/com/study/boardback/config/WebSecurityConfig.java
@@ -0,0 +1,83 @@
+package com.study.boardback.config;
+
+import com.study.boardback.filter.JwtAuthenticationFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Configurable
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    //  CORS 설정
+    private CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:3000")); // ⭐️ 허용할 origin
+            config.setAllowCredentials(true);
+            return config;
+        };
+    }
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/", "/api/v1/auth/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/v1/board/**").permitAll()
+                                .requestMatchers("/api/v1/search/**").permitAll()
+                                .requestMatchers("/api/v1/user/*").permitAll()
+                                .requestMatchers("/file/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .exceptionHandling(exceptionHandlingConfigurer ->
+                        exceptionHandlingConfigurer.authenticationEntryPoint(new FailedAuthenticationEntryPoint()))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+
+}
+
+class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.getWriter().write("{\"code\": \"NP\", \"message\": \"Do not have permission.\"}");
+
+    }
+
+}
---
+++ src/main/java/com/study/boardback/provider/JwtProvider.java
@@ -0,0 +1,41 @@
+package com.study.boardback.provider;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    public String create(String email){
+        Date expireDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS)); // 1시간
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .compact();
+        return jwt;
+    }
+
+    public String validate(String jwt){
+        Claims claims = null;
+        try {
+            claims = Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(jwt).getBody();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+        return claims.getSubject();
+    }
+}
